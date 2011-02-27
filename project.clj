5a2fe76377465afb9a8a31a8a7399e159c0c7807
Daniel Jomphe <danieljomphe@gmail.com>
2011-02-27 18:54:59 -0500

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ca.slu.spdv "1.0.0-SNAPSHOT"
+(defproject ca.slu.spdv "0.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
