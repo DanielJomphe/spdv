7a33122ee26893952fc2d2f867f564cd1f2f9fae
Daniel Jomphe <danieljomphe@gmail.com>
2011-02-27 16:55:06 -0500

    first commit

---
+++ src/ca/slu/spdv/core.clj
@@ -0,0 +1,1 @@
+(ns ca.slu.spdv.core)
---
+++ test/ca/slu/spdv/test/core.clj
@@ -0,0 +1,6 @@
+(ns ca.slu.spdv.test.core
+  (:use [ca.slu.spdv.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
