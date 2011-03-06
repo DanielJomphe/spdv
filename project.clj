2842ff87b4c63a23cf295932e8f89cdd9d0e1bac
Daniel Jomphe <danieljomphe@gmail.com>
2011-03-06 18:09:43 -0500

    dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject ca.slu.spdv "0.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.h2database/h2 "1.3.152"]
+                 [lobos "0.7.0-SNAPSHOT"]
+                 [clojureql "1.0.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+                     [marginalia "0.5.0-alpha"]])
