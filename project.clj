c541ba44258a2beb38188e036c353c7c16b9aeb9
Daniel Jomphe <danieljomphe@gmail.com>
2011-03-20 14:39:39 -0400

    startup with hazelcast; upgrade h2database to 1.3

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject ca.slu.spdv "0.1.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "SPDV: FIXME..."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.h2database/h2 "1.3.152"]
+                 [apparatus "1.0.0-SNAPSHOT"]
+                 [com.h2database/h2 "1.3.153"]
                  [lobos "0.7.0-SNAPSHOT"]
                  [clojureql "1.0.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]
-                     [marginalia "0.5.0-alpha"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [marginalia "0.5.0-alpha"]]
+  :aot [apparatus.eval]
+  :main apparatus.main)
