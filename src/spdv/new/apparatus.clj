89cbb9bdefb491a4ca0506aa5e0f4186c0c7852a
Daniel Jomphe <danieljomphe@gmail.com>
2011-06-11 17:44:18 -0400

    fix thing broken by hazelcast; streamline crud macros

--- src/spdv/new/apparatus.clj
+++ src/spdv/new/apparatus.clj
@@ -74,49 +74,49 @@
   (-> (Hazelcast/getIdGenerator "hz-instance-ids") (.newId)))
 
 ;;; Hazelcast Query: Predicate
+;;; I stopped using it. For some reason, (.values pred-all) became long-running!?, so I instead now use (.values).
 (def pred-all
   (reify Predicate
     (apply [_ _] true)))
 
 ;;; CRUD for Hazelcast data structures
-(defn symbol-crud [name op]
-  (symbol (str name "-" op)))
+(defn make-symbol [before-dash after-dash]
+  (symbol (str before-dash "-" after-dash)))
 
-(comment
-  (use 'clojure.pprint))
-
-(defmacro defcrudop                     ;support diff arities?
-                                        ;might also not -> auto
-  [hz-ds name op bindings & forms]
-  `(defn ~(symbol-crud name op)
-     [~@bindings]
+(defmacro defcrudop [hz-ds name op bindings & forms]
+  `(defn ~(make-symbol name op) [~@bindings]
      (-> ~hz-ds ~@forms)))
 
-(comment
-  (pprint (macroexpand-1 '(defcrudop (get-map "services") "services" "list"
-                            [] (.values pred-all))))
-  -->
-  (defn services-list
-    [] (-> (get-map "services") (.values pred-all))))
+(defmacro make-crudop [& rest]
+  "This macro depends on its local context to provide all vars it references except 'rest'."
+  `(defcrudop (~'ap-ds-getter ~'name) ~'name ~@rest))
 
-;; alternatively, could map on ops & bindings & forms, with default
-;; ops being the usual crud : ["list" "put" "get" "delete"]
-;; or better?: doseq on [{:op "list" :bindings [] :forms ...}...]
-(defmacro defcrud                ;stop supporting diff crudop-deffers?
-  [crudop-deffer ap-ds-getter name]
-  `(do
-     (~crudop-deffer (~ap-ds-getter ~name) ~name "put"
-                     [k# v#] (.put    k# v#))
-     (~crudop-deffer (~ap-ds-getter ~name) ~name "list"
-                     []      (.values pred-all))
-     (~crudop-deffer (~ap-ds-getter ~name) ~name "get"
-                     [k#]    (.get    k#))
-     (~crudop-deffer (~ap-ds-getter ~name) ~name "delete"
-                     [k#]    (.remove k#))))
+(defmacro defcrud [ap-ds-getter name]
+  `(do ~(make-crudop "put"    [k# v#] (.put    k# v#))
+       ~(make-crudop "list"   [     ] (.values      ))
+       ~(make-crudop "get"    [k#   ] (.get    k#   ))
+       ~(make-crudop "delete" [k#   ] (.remove k#   ))))
 
 (comment
-  (pprint (macroexpand-1 '(defcrud defcrudop get-map "services")))
-  -->
-  (do (...only showing below the R of CRUD...)
-    (defcrudop (get-map "services") "services" "get"
-      [k#] (.get k#))))
+  (defn macprint [form]
+    (do
+      (println "==============")
+      (use 'clojure.pprint)
+      (pprint (macroexpand-1 form))))
+
+  (macprint '(defcrudop (get-map "my-map") "my-map" "get" [k#] (.get k#)))
+  (macprint '(make-crudop "get" [k#] (.get k#)))
+  (macprint '(defcrud get-map "my-map"))
+
+  (comment --> (defn my-map-get [k#]
+                 (-> (get-map "my-map") (.get k#)))
+           --> (defcrudop (get-map "my-map") "my-map" "get" [k#] (.get k#))
+           --> (do ...
+                   (make-crudop "get" [k#] (.get k#))
+                   ...))
+
+  (defcrud get-map "blah")
+  (blah-list)
+  (blah-put :k :v)
+  (blah-get :k)
+  (blah-delete :k))
